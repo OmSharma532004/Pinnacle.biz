ea091e72bfa0b34e0c5ef1b4750d60ad31e042ef
OmSharma532004 <22103203@mail.jiit.ac.in>
2024-07-27 17:55:39 +0530

    Done

--- cllient/src/Pages/Hr360/Components/Header/Header.jsx
+++ cllient/src/Pages/Hr360/Components/Header/Header.jsx
@@ -1,11 +1,10 @@
-import "./Header.scss"
-import { AppWrap } from "../../../../Wrapper"
-import  {motion} from "framer-motion"
-import {images} from "../../../../constants"
+import "./Header.scss";
+import { AppWrap } from "../../../../Wrapper";
+import { motion } from "framer-motion";
+import { images } from "../../../../constants";
 import Typewriter from "typewriter-effect";
 import { Link } from 'react-router-dom';
-
-import anchor from "../../../../assets/anchor.png"
+import anchor from "../../../../assets/anchor.png";
 
 const scalevarients = {
   whileInView: {
@@ -20,81 +19,45 @@ const scalevarients = {
 
 const Header = () => {
   return (
-    <div className='app__header app__flex mt-[10px]  md:mt-[0px]' id="home">
+    <div className='app__header app__flex mt-[10px] md:mt-[0px]' id="home">
       <motion.div 
-          whileInView={{x: [-100,0], opacity: [0,1]}}
-          className='app__header-info'
-          transition={{duration: 0.5, delayChildren: 0.5}}
+        whileInView={{x: [-100, 0], opacity: [0, 1]}}
+        className='app__header-info'
+        transition={{duration: 0.5, delayChildren: 0.5}}
       >       
-
-          <header className="header">
-                        
-           <div className=" flex  md:mt-8 lg:mt-8 items-center  md:gap-[40px]">
-           <h1 className="headline text-blue-800 head-text">
-           HR360
-              {/* <span>
-              <h4> Reach New Heights</h4>
-              </span> */}
-            
+        <header className="header">
+          <div className="flex md:mt-8 lg:mt-8 items-center md:gap-[40px]">
+            <h1 className="headline text-blue-800 head-text">
+              HR360
             </h1>
-            {/* <img src={anchor} alt="profile" className="profile" width={100} /> */}
-           </div>
-
-            <p className="text-lg">
-
-              Set Yourself Apart and Achieve Your Career Goals with Our Support
-            </p>
-            <p className='header-content -z-2'>
-Pinnacle is a well-established talent acquisition, talent management, and executive search firm
-with over 14 years of experience. We have successfully partnered with hundreds of companies
-and assisted thousands of candidates across three continents, working with organizations of all
-sizes—from bootstrapped startups to growth-phase firms and unicorns.
-Our expertise lies in connecting top-tier talent with leading organizations, offering
-comprehensive 360-degree talent solutions. We are at the cutting edge of emerging industry
-trends, and our team comprises some of the best minds in the business, bringing a wealth of
-global experience to our clients.</p>
-
-
-            <div className='header-buttons '>
-            <Link to="https://calendly.com/contact-pinnacle/book-a-meeting"
-                  rel="noreferrer"              
-                  className="bg-yellow-400 p-4 rounded-lg text-blue-800  text-lg">
-                  Get Started
-                </Link>
-            </div>
-       
-          </header>
-          
-          <motion.img
-           whileInView={{scale: [0,1]}}
-           transition={{duration: 1, ease: "easeInOut"}}
-           src={images.circle}
-           alt="profile_circle"
-           className="overlay_circle"
+          </div>
+          <h2 className=" head-text headline   text-blue-800 mb-8">Managing your superstars</h2>
+          <p className="text-lg mb-6 text-black">
+            For all things related to payroll, hiring, taxation, compliance, insurance, and more!
+          </p>
+          <p className="text-lg mb-6 text-black">
+            HR 360 is a game-changing solution designed to transform your hiring process, optimize your workforce, and significantly reduce manpower costs. Our Employer of Record (EOR) services are crafted to streamline your HR operations, allowing you to focus on what truly matters—growing your business.
+          </p>
+          <p className="text-lg mb-6 text-black">
+            We pride ourselves on our successful partnerships with clients, delivering unparalleled HR solutions that meet your unique needs. With our HR360, you gain access to a comprehensive suite of services that simplify employment complexities, enhance compliance, and drive efficiency.
+          </p>
+          <p className="text-lg mb-6 text-black">
+            Experience the difference with HR360 and discover how our innovative EOR services can elevate your business to new heights.
+          </p>
+          <Link to="https://calendly.com/contact-pinnacle/book-a-meeting" rel="noreferrer" className="bg-yellow-400 mt-4 p-4 rounded-lg text-blue-800 text-lg">
+            Get Started
+          </Link>
+        </header>
+        <motion.img
+          whileInView={{scale: [0, 1]}}
+          transition={{duration: 1, ease: "easeInOut"}}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
         />
-
-
-
-      
       </motion.div>
-      
-      
-      {/* <motion.div
-        variant={scalevarients}
-        whileInView={scalevarients.whileInView}
-        className="app__header-circles"
-      >
-        {[Rocket,handshake, trophy].map((circle, index) => (
-            <div className="circle-cmp app__flex" key={`circle-${index}`}>
-              <img src={circle} alt="profile_bg" />
-            </div>
-        ))}
-
-      </motion.div> */}
     </div>
   )
 }
-
-// export default Header;
 
 export default Header;
--- cllient/src/Pages/Hr360/HR360.jsx
+++ cllient/src/Pages/Hr360/HR360.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import Header from './Components/Header/Header';
-import About2 from './Components/About2/About';
 import Footer from '../../components/Footer/Footer';
 import Navbar from '../../components/Navbar/Navbar';
-import useCase from './Components/assets/image.png'
+import useCase from './Components/assets/image.png';
+import Header from './Components/Header/Header';
+import About2 from './Components/About2/About';
 
 const Hr360 = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -23,50 +23,46 @@ const Hr360 = () => {
   return (
     <div className="app bg-white text-gray-800 font-sans">
       <Navbar />
-      <div className="mt-12"></div>
       <Header />
-    
-      
-      <main className="container mx-auto p-6">
+
+      <main className=" mx-auto px-6 py-12">
         <div className="flex flex-col items-center justify-center space-y-12">
-          <section className="bg-white p-6 rounded border-t-4 border-blue-800">
+          <section className="bg-white p-8 rounded-xl shadow-md border-t-4 border-blue-800 w-full md:w-3/4">
             <div>
-              <h2 className="md:text-5xl text-3xl font-bold mb-4 text-blue-800 ">HR360’s Success Story of Holistic Solutions</h2>
-              <h3 className="text-lg mb-6 text-black r">for a reputed fintech startup serving the Middle East & North Africa region</h3>
+              <h2 className="md:text-5xl text-3xl font-bold mb-6 text-blue-800 text-center">HR360’s Success Story of Holistic Solutions</h2>
+              <h3 className="text-lg mb-6 text-black">The following case study pertains to a reputed fintech startup that operates within the Middle East and North Africa (MENA) region.</h3>
+              <p className="mb-6 text-lg text-black">Client: Leading Fintech Startup serving the MENA region</p>
+              <h3 className="text-3xl md:text-5xl font-bold mt-12 mb-6 text-blue-800">Problem Statement:</h3>
+              <ul className="list-disc text-lg list-inside mb-6 text-gray-800">
+                <li>Time for liquidity crunch across VC/PE investments</li>
+                <li>The dilemma was to continue product innovation while keeping a check on the cost</li>
+              </ul>
+              <img src={useCase} alt="use-case" className="rounded-xl hidden md:flex mt-6 w-full border-t-4 border-l-2 border-b-4 border-black mb-6" />
             </div>
-            <p className="mb-6 text-lg  text-black ">Client: Leading Fintech Startup serving the MENA region</p>
-            <h3 className="text-3xl md:text-5xl  font-bold mt-12 mb-4 text-blue-800 r">Problem Statement:</h3>
-            <ul className="list-disc text-lg list-inside mb-6 text-gray-800 r">
-              <li>Time for liquidity crunch across VC/PE investments</li>
-              <li>The dilemma was to continue product innovation while keeping a check on the cost</li>
-            </ul>
           </section>
 
-         
-
-          <img src={useCase
-            } alt="use-case" className=" hidden md:flex rounded-xl w-full border-t-4 border-l-2 border-b-4 border-black" />
-          <section className="bg-white p-6 w-5/6 rounded border-t-4 border-blue-800">
-            <h3 className="text-3xl md:text-5xl t font-bold mb-4 text-blue-800 ">Voice of Stakeholders</h3>
-            <blockquote className="mb-4 text-lg text-gray-800 ">
+          <section className="bg-white p-8 rounded-xl shadow-md border-t-4 border-blue-800 w-full md:w-3/4">
+            <h3 className="text-3xl md:text-5xl font-bold mb-6 text-blue-800 text-center">Voice of Stakeholders</h3>
+            <blockquote className="mb-6 text-lg text-gray-800">
               "Game-changer for our startup! The exceptional services catapulted our growth and made a real impact." ~ Our client
             </blockquote>
-            <blockquote className=" text-lg text-gray-800 ">
+            <blockquote className="text-lg text-gray-800">
               "The team is highly responsive and always there to assist us with any issues. Regular connects & engagements from their side have helped me drive 100% retention for my team in India." ~ Engineering Leader for India
             </blockquote>
           </section>
-        </div>
-      </main>
-      <section className=" flex flex-col mb-[40px] items-center justify-center">
-      <h2 className="md:text-5xl text-3xl md:-ml-[800px] text-blue-800 font-bold">Why Us</h2>
+
+          <section className="bg-white p-8 rounded-xl shadow-md border-t-4 border-blue-800 w-full md:w-3/4">
+            <h2 className="md:text-5xl text-3xl text-blue-800 font-bold mb-6 text-center">Why Choose Us</h2>
             <About2 />
           </section>
-    
-      
+        </div>
+      </main>
+
       <Footer />
-      <div className="fixed bottom-5 right-5 z-10 md:bottom-36 md:right-10">
+
+      <div className="fixed right-5 z-10 bottom-44 md:right-10">
         <a 
-          className="text-lg font-semibold hover:bg-yellow-600 bg-yellow-400 rounded-xl p-5 text-blue-800" 
+          className="text-lg font-semibold hover:bg-yellow-600 bg-yellow-400 rounded-xl p-4 text-blue-800" 
           href="https://calendly.com/contact-pinnacle/book-a-meeting"
         >
           Schedule a Meeting
