4b05e56cf1be93c45696bef9e420e901aed865fb
OmSharma532004 <22103203@mail.jiit.ac.in>
2024-08-01 23:15:25 +0530

    Done

--- cllient/src/Pages/Cotingency/ContingencySearch.jsx
+++ cllient/src/Pages/Cotingency/ContingencySearch.jsx
@@ -8,11 +8,12 @@ const contingencyData = {
     description: "In the context of recruitments, contingent stands for conversion/closure-based fees that Pinnacle charges our clients on successfully filling your organization’s open position.",
     advantages: [
       "Reduced fees: With contingency hiring you only have to pay for the successful hire that you make using our platforms.",
+      "No long-term commitments: We operate on a project-to-project basis so you don’t have to commit any funds towards retaining us as a partner.",
+      "Quality guarantee: With Pinnacle’s experience and industry expertise you can be rest assured that you will always be able to hire the most relevant candidate for your open position.",
+   
       "Quick turnaround: If your organization needs to fulfil a role quickly you should most certainly assess this option to onboard the right candidate for the role without losing much time. Every day, week, or month where a critical position remains vacant you lose much more money as a business.",
       "No need for in house recruiters/ recruitment teams: Recruitments are often not an annual process and generally an organization hires when it scales up or when it needs to back fill for a role. Outsourcing your recruiting efforts means you don’t have to set up full-fledged recruiting/ talent acquisition teams and bear their cost perennially.",
-      "No long-term commitments: We operate on a project-to-project basis so you don’t have to commit any funds towards retaining us as a partner.",
-      "Quality guarantee: With Pinnacle’s experience and industry expertise you can be rest assured that you will always be able to hire the most relevant candidate for your open position."
-    ],
+       ],
     contact: "For more information on how Pinnacle can assist with your recruitment needs please contact us at contact@pinnacle.biz"
   };
 
--- cllient/src/Pages/Research'/ResearchAdv.jsx
+++ cllient/src/Pages/Research'/ResearchAdv.jsx
@@ -7,11 +7,12 @@ const researchAndAdvisoryData = {
     description: "At Pinnacle, we believe that informed decision-making is the cornerstone of successful business strategies. Our Research and Advisory services are designed to provide you with the insights and guidance needed to navigate complex business landscapes and drive sustainable growth.",
     ctaButton: "Get Started",
     advantages: [
-      "Actionable Insights: With over 14 years of experience, we have developed a deep understanding of various industries. This enables us to derive research-driven insights to assist you in gaining a competitive edge, guiding your strategic decisions, and effectively navigating challenges.",
-      "Expert Analysis: Access thorough analysis of market trends, consumer behavior, and competitive landscapes to stay ahead in your industry.",
+          "Expert Analysis: Access thorough analysis of market trends, consumer behavior, and competitive landscapes to stay ahead in your industry.",
+       "Customized Solutions: Our tailored solutions are designed specifically to address your unique business needs and objectives.",
+      "Track Record of Success: Expect a proven track record of delivering valuable research and advisory services that drive tangible results for our clients.",
+      "Actionable Insights: With over 14 years of experience, we have developed a deep understanding of various industries. This enables us to derive research-driven insights to assist you in gaining a competitive edge.",
       "Enhanced Decision-Making: Receive guidance in the identification and mitigation of risks, enabling you to make well-informed decisions and avoid potential pitfalls.",
-      "Customized Solutions: Our tailored solutions are designed specifically to address your unique business needs and objectives.",
-      "Track Record of Success: Expect a proven track record of delivering valuable research and advisory services that drive tangible results for our clients."
+   
     ],
     contact: "Ready to leverage research and advisory services to enhance your business strategy and decision-making? Contact our team at Pinnacle today to learn more about how our expertise can help you achieve your goals."
   };
